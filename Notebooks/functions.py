d8b32c68d3ee3a75473787279f0227794547769b
Rafal Fedro <rafal.fedro@gmail.com>
2023-10-25 12:18:34 +0200

    Added EDA, scratchpad and functions files.

---
+++ Notebooks/functions.py
@@ -0,0 +1,26 @@
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# import modules
+import pandas as pd
+import numpy as np
+import pickle
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# horizontal divider line
+
+def f_divline(pad_before=True, pad_after=True):
+    '''Draws (prints) a simple horizontal line
+    Args.:
+        pad_before (bool): add pading before the line
+        pad_after (bool): add padding after the line
+    '''
+    line = str('- '*40)
+    if pad_before:
+        line = '\n'+line
+    if pad_after:
+        line = line+'\n'
+    
+    return print(line)
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
